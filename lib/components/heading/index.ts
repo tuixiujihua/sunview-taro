945815fcd515b0c1a1f9d196cae9503e15754c2f
ilover <admin@ilover.me>
2021-04-22 16:03:48 +0800

    更新了heading组件

--- lib/components/heading/index.ts
+++ lib/components/heading/index.ts
@@ -10,16 +10,37 @@ export default {
 			validator: (val) => [
 				1, 2, 3, 4, 5, 6, 7, 8, 9
 			].includes(val)
-		}
+		},
+		line: {
+			type: Boolean,
+			default: false
+		},
+		lineType: {
+			type: String,
+			default: "default",
+			validator: (val) => ["default", "primary", "success", "warning", "danger"].includes(val)
+		},
+		type: {
+			type: String,
+			default: "default",
+			validator: (val) => ["default", "primary", "success", "warning", "danger"].includes(val)
+		},
 	},
 	setup(props, { attrs, slots }) {
 		return () => h(View, mergeProps({
 			class: [
 				"s-heading",
-				`s-heading-level-${props.level}`
+				`s-heading-level-${props.level}`,
+				props.line ? 's-heading-border-line' : '',
+				props.line ? `s-heading-border-line-${props.lineType}` : '',
 			],
-		}, attrs), {
+		}, attrs), h(View, {
+			class: [
+				's-heading-context',
+				`s-heading-context-${props.type}`
+			]
+		}, {
 			default: () => slots.default?.()
-		})
+		}))
 	}
 }
--- src/pages/heading/index.ts
+++ src/pages/heading/index.ts
@@ -5,11 +5,42 @@ import './index.scss'
 export default {
 	setup() {
 
-		let level = [1, 2, 3, 4, 5, 6, 7];
+		let level = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 		let children: Array<any> = [];
 
 		level.map((value, key) => {
 			children.push(h(SHeading, { level: value }, { default: () => "我是一个标题" }));
+		})
+		level.map((value, key) => {
+			children.push(h(SHeading, { level: value, line: true }, { default: () => "我是一个标题" }));
+		})
+		level.map((value, key) => {
+			children.push(h(SHeading, { level: value, line: true, lineType: 'primary' }, { default: () => "我是一个标题" }));
+		})
+		level.map((value, key) => {
+			children.push(h(SHeading, { level: value, line: true, lineType: 'success' }, { default: () => "我是一个标题" }));
+		})
+		level.map((value, key) => {
+			children.push(h(SHeading, { level: value, line: true, lineType: 'warning' }, { default: () => "我是一个标题" }));
+		})
+		level.map((value, key) => {
+			children.push(h(SHeading, { level: value, line: true, lineType: 'danger' }, { default: () => "我是一个标题" }));
+		})
+
+		level.map((value, key) => {
+			children.push(h(SHeading, { level: value, line: true, type: "primary" }, { default: () => "我是一个标题" }));
+		})
+
+		level.map((value, key) => {
+			children.push(h(SHeading, { level: value, line: true, type: "success" }, { default: () => "我是一个标题" }));
+		})
+
+		level.map((value, key) => {
+			children.push(h(SHeading, { level: value, line: true, type: "warning" }, { default: () => "我是一个标题" }));
+		})
+
+		level.map((value, key) => {
+			children.push(h(SHeading, { level: value, line: true, type: "danger" }, { default: () => "我是一个标题" }));
 		})
 
 		return () => h(View, { class: 'page-heading' }, {
