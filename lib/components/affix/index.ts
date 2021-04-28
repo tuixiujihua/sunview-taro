f4f50ad814fe0b83d59641cd05181e15afa401b8
ilover <admin@ilover.me>
2021-04-29 01:17:23 +0800

    完成affix组件

--- lib/components/affix/index.ts
+++ lib/components/affix/index.ts
@@ -1,6 +1,6 @@
 import { View } from "@tarojs/components"
 import Taro, { nextTick } from '@tarojs/taro'
-import { h, mergeProps, ref, watch, watchEffect, computed, getCurrentInstance, handleError } from "@vue/runtime-core"
+import { h, mergeProps, ref, watch, computed } from "@vue/runtime-core"
 import { createRandomNumber } from '../../utils'
 import './index.scss'
 
@@ -42,19 +42,8 @@ export default {
 		let handler = () => {
 			elmQuery.exec(res => {
 				detail.value = { ...res[0] };
-				// console.log(detail.value);
-				// console.log(computedTrigger.value);
-				// console.log(res[2].dataset.scrollTop);
-
-				// if (computedTrigger.value.)
-				// console.log(res[1], detail.value.top, detail.value.bottom)
 				fixedTop.value = detail.value.bottom - detail.value.height <= props.top ? true : false;
-
-				fixedBottom.value = detail.value.bottom  <= res[1].height - props.bottom ? true : false;
-				console.log(detail.value, res[1],res[2].scrollTop);
-				// if (fixedTop.value) {
-				// 	console.log(detail.value,res[2].scrollTop + detail.value.height, props.top);
-				// }
+				fixedBottom.value = res[1].height - detail.value.top - detail.value.height <= props.bottom ? true : false;
 			})
 		}
 
@@ -77,7 +66,6 @@ export default {
 					visibility: fixedTop.value || fixedBottom.value ? 'hidden' : 'visible'
 				}
 			}, slots.default?.()),
-			// true ? h(View, {
 			fixedTop.value || fixedBottom.value ? h(View, {
 				class: "s-affix-fixed",
 				style: {
@@ -85,59 +73,9 @@ export default {
 					height: `${detail.value.height}px`,
 					top: fixedTop.value ? `${props.top}px` : null,
 					bottom: fixedBottom.value ? `${props.bottom}px` : null
-					// top: null `${props.top}px`
 				}
 			}, slots.default?.()) : ""
 		])
 
 	}
-
-	// setup(props, { attrs, slots }) {
-
-	// 	// let ctx = getCurrentInstance();
-
-	// 	let computedTrigger = computed(() => {
-	// 		return props.trigger
-	// 	})
-
-	// 	return { computedTrigger }
-
-	// },
-	// data() {
-	// 	return {
-	// 		uniKey: createRandomNumber(1000, 9999),
-	// 		detail: {}
-	// 	}
-	// },
-
-	// render() {
-	// 	console.log("fasdfasfasd", this);
-	// 	return h(View, mergeProps({
-	// 		id: `s-affix-${this.uniKey}`,
-	// 		class: ["s-affix"],
-	// 	}, this.$attrs), [
-	// 		this.$slots.default?.(),
-	// 		h(View, {
-	// 			style: {
-	// 				width: `${this.detail.width || 0}px`,
-	// 				height: `${this.detail.height || 0}px`
-	// 			},
-	// 			class: ['s-affix-fixed']
-	// 		}, this.$slots.default?.())
-	// 	])
-	// },
-
-	// mounted() {
-	// 	nextTick(() => {
-	// 		const query = Taro.createSelectorQuery()
-	// 		query.select(`#s-affix-${this.uniKey}`).boundingClientRect()
-	// 		query.exec(function (res) {
-	// 			if (res) {
-	// 				console.log(res[0]);
-	// 				this.detail = res[0];
-	// 			}
-	// 		})
-	// 	});
-
-	// }
 }
